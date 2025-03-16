2fe00cf548cad3605bf2227468ec1a1c9512e4c8
ricpaula <cadodepaulasilva159@gmail.com>
2025-03-16 16:02:56 -0300

    Update Colors

--- js/main.js
+++ js/main.js
@@ -162,7 +162,7 @@ function colorMode() {
       box.classList.add("box-lightmode");
     });
   } else {
-    document.body.style.backgroundColor = "#040D12";
+    document.body.style.backgroundColor = "#0f0f0f";
     let allH1 = document.querySelectorAll("h1");
     allH1.forEach((h1) => {
       h1.classList.remove("text-lightmode");
